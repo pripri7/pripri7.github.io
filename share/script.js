a36d105172638e2114ba6cbb64a44d3e57680b7c
d <d>
2024-09-08 16:45:58 +0700

    update

---
+++ share/script.js
@@ -0,0 +1,27 @@
+const downloadBtn = document.querySelector(".download-btn");
+const fileLink = "https://mbehhh.github.io/share/DGTVBRO.apk";
+
+const initTimer = () => {
+  if (downloadBtn.classList.contains("disable-timer")) {
+    return location.href = fileLink;
+  }
+  let timer = downloadBtn.dataset.timer;
+  downloadBtn.classList.add("timer");
+  downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+  const initCounter = setInterval(() => {
+    if (timer > 0) {
+      timer--;
+      return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+    }
+    clearInterval(initCounter);
+    location.href = fileLink;
+    downloadBtn.innerText = "Your file is downloading...";
+    setTimeout(() => {
+      downloadBtn.classList.replace("timer", "disable-timer");
+      downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
+                                     <span class="text">Download Again</span>`;
+    }, 3000);
+  }, 1000);
+}
+
+downloadBtn.addEventListener("click", initTimer);
